29c10199c6432aa4db467b3f843b6bf8d3788a41
unknown <coding812@gmail.com>
2024-06-26 17:28:31 -0400

    Set up home page basic layout from template

---
+++ src/app/Contact/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div className='flex min-h-screen'>
+            <h1>Contact Page</h1>
+            {/* Add your contact form or other content here */}
+        </div>
+    );
+};
+
+export default Contact;
