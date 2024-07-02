2e3e3086889714a1c91b12ccd3e95a09b1b4d0a4
unknown <coding812@gmail.com>
2024-07-02 16:51:31 -0400

    Added new responsive navbar

--- src/app/Components/Navbar.js
+++ src/app/Components/Navbar.js
@@ -1,8 +1,47 @@
 "use client";
 import React from 'react';
+import { useState } from 'react'
 import { motion } from "framer-motion";
+import {
+    Dialog,
+    DialogPanel,
+    Disclosure,
+    DisclosureButton,
+    DisclosurePanel,
+    Popover,
+    PopoverButton,
+    PopoverGroup,
+    PopoverPanel,
+} from '@headlessui/react'
+import {
+    ArrowPathIcon,
+    Bars3Icon,
+    ChartPieIcon,
+    CursorArrowRaysIcon,
+    FingerPrintIcon,
+    SquaresPlusIcon,
+    XMarkIcon,
+} from '@heroicons/react/24/outline'
+import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
+
+const products = [
+    { name: 'Analytics', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon },
+    { name: 'Engagement', description: 'Speak directly to your customers', href: '#', icon: CursorArrowRaysIcon },
+    { name: 'Security', description: 'Your customers’ data will be safe and secure', href: '#', icon: FingerPrintIcon },
+    { name: 'Integrations', description: 'Connect with third-party tools', href: '#', icon: SquaresPlusIcon },
+    { name: 'Automations', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
+]
+const callsToAction = [
+    { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
+    { name: 'Contact sales', href: '#', icon: PhoneIcon },
+]
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
 
 function Navbar() {
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
     const variants = {
         initial: { scale: 0 },
         animate: { scale: 1 },
@@ -28,81 +67,100 @@ function Navbar() {
         }
     }
     return (
-            
-                <motion.div
-                    initial="initial"
-                    animate="animate"
-                    variants={variants.parent}
-                    className="flex justify-center p-8 w-full ">
-                    <motion.a
-                        variants={variants}
-                        href="Writings"
-                        className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                        rel="noopener noreferrer"
+        <header className="bg-white bg-opacity-10 backdrop-blur-lg rounded-lg drop-shadow-lg sticky top-0">
+            <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+                <div className="flex lg:flex-1">
+                    <a href="#" className="-m-1.5 p-1.5">
+                        <span className="sr-only">Your Company</span>
+                        <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+                    </a>
+                </div>
+                <div className="flex lg:hidden">
+                    <button
+                        type="button"
+                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                        onClick={() => setMobileMenuOpen(true)}
                     >
-                        <h2 className={`mb-3 text-2xl font-semibold`}>
-                            Writings{" "}
-                            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                                -&gt;
-                            </span>
-                        </h2>
-                        <p className={`m-0 max-w-[20ch] text-sm opacity-50`}>
-                            Some of Mike's Featured Works.
-                        </p>
-                    </motion.a>
+                        <span className="sr-only">Open main menu</span>
+                        <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+                    </button>
+                </div>
+                <PopoverGroup className="hidden lg:flex lg:gap-x-12">
 
-                    <motion.a
-                        variants={variants}
-                        href="About"
-                        className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-                        rel="noopener noreferrer"
-                    >
-                        <h2 className={`mb-3 text-2xl font-semibold`}>
-                            About{" "}
-                            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                                -&gt;
-                            </span>
-                        </h2>
-                        <p className={`m-0 max-w-[20ch] text-sm opacity-50`}>
-                            More about Mike.
-                        </p>
-                    </motion.a>
-
-                    <motion.a
-                        variants={variants}
-                        href="/Music"
-                        className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                        rel="noopener noreferrer"
-                    >
-                        <h2 className={`mb-3 text-2xl font-semibold`}>
-                            Music{" "}
-                            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                                -&gt;
-                            </span>
-                        </h2>
-                        <p className={`m-0 max-w-[20ch] text-sm opacity-50`}>
-                            Explore some of Mikes's musical endeavors.
-                        </p>
-                    </motion.a>
-
-                    <motion.a
-                        variants={variants}
-                        href="/Contact"
-                        className="group rounded-lg border border-transparent px-2 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-
-                        rel="noopener noreferrer"
-                    >
-                        <h2 className={`mb-3 text-2xl font-semibold`}>
-                            Contact{" "}
-                            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                                -&gt;
-                            </span>
-                        </h2>
-                        <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-                            Get in touch
-                        </p>
-                    </motion.a>
-                </motion.div>
+                    
+                    <a href="/Writings" className="text-sm font-semibold leading-6 text-gray-900">
+                        Writings
+                    </a>
+                    <a href="/Music" className="text-sm font-semibold leading-6 text-gray-900">
+                        Music
+                    </a>
+                    <a href="/About" className="text-sm font-semibold leading-6 text-gray-900">
+                        About
+                    </a>
+                </PopoverGroup>
+                <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+                    <a href="Contact" className="text-sm font-semibold leading-6 text-gray-900">
+                        Contact <span aria-hidden="true">&rarr;</span>
+                    </a>
+                </div>
+            </nav>
+            {/* BEGIN MOBILE MENU */}
+            <Dialog className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+                <div className="fixed inset-0 z-10" />
+                <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white bg-opacity-10 backdrop-blur-lg rounded-lg drop-shadow-lg px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                    <div className="flex items-center justify-between">
+                        <a href="#" className="-m-1.5 p-1.5">
+                            <span className="sr-only">Your Company</span>
+                            <img
+                                className="h-8 w-auto"
+                                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                                alt=""
+                            />
+                        </a>
+                        <button
+                            type="button"
+                            className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                            onClick={() => setMobileMenuOpen(false)}
+                        >
+                            <span className="sr-only">Close menu</span>
+                            <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                        </button>
+                    </div>
+                    <div className="mt-6 flow-root">
+                        <div className="-my-6 divide-y divide-gray-500/10">
+                            <div className="space-y-2 py-6">
+                                <a
+                                    href="Writings"
+                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                >
+                                    Writings
+                                </a>
+                                <a
+                                    href="About"
+                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                >
+                                    About
+                                </a>
+                                <a
+                                    href="Music"
+                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                >
+                                    Music
+                                </a>
+                            </div>
+                            <div className="py-6">
+                                <a
+                                    href="Contact"
+                                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                >
+                                    Contact
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </DialogPanel>
+            </Dialog>
+        </header>
     );
 }
 
